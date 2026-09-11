af5deb462a60033e8927401e32fde57876fbed1a
Nicolas <nicolsspanhol15@gmail.com>
2026-09-10 22:05:02 -0300

    atividade escolha

--- jogoEscolha.py
+++ jogoEscolha.py
@@ -2,21 +2,21 @@ import random
 
 print("Jogo da Escolha!")
 
+numero_pc = random.randint(1, 10)
+
 for i in range (3):
     usuario = int(input("Escolha um número de 1 a 10: "))
 
-    if usuario > 10 or usuario == 0:
-        print("Escolha errada, tente novamente!")
+    if usuario < 1 or usuario > 10:
+        print("Escolha errada, tente novamente com um número de 1 a 10!")
         continue
     
-numero_pc = random.randint(1, 10)
-
-if usuario == numero_pc:
-    print("Você acertou!")
-else: 
-    print("Você errou, tente novamente!")
+    if usuario == numero_pc:
+        print("Você acertou!")
+        break
+    else: 
+        print("Você errou, tente novamente!")
     
-
 print("O PC escolheu: ", numero_pc)
 
 
